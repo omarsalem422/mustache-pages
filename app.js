d90a32c0154e11ae8f58038d27de6b7695a8471d
Omar <omar@omarsalempro.com>
2020-02-20 17:34:12 -0700

    Add code to inject data in the mustache page

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ app.set('view engine', 'mustache');
 
 const PORT = process.env.PORT || 3000;
 
+const user = {
+  name: "John Doe", 
+  address: {
+    street : "13433 Race Street",
+    city: "Thornton",
+    state: "CO",
+    zipCode:  "80241"
+  }
+  }
 app.get('/', (req,res) => {
-  res.render('index');
+  res.render('index', user);
 });
 
 app.listen(PORT, () => {
