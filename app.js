93eb3faeeb75a94d96414a8ff49ec868e7228494
Omar <omar@omarsalempro.com>
2020-02-22 11:36:17 -0700

    Add code for displaying list of users in mustache page

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mustacheExpress = require('mustache-express');
+const mustacheExpress = require("mustache-express");
 
-app.engine('mustache', mustacheExpress());
-app.set('views', './views');
-app.set('view engine', 'mustache');
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
 
 const PORT = process.env.PORT || 3000;
 
 const user = {
-  name: "John Doe", 
+  name: "John Doe",
   address: {
-    street : "13433 Race Street",
+    street: "13433 Race Street",
     city: "Thornton",
     state: "CO",
-    zipCode:  "80241"
+    zipCode: "80241"
   }
-  }
-app.get('/', (req,res) => {
-  res.render('index', user);
+};
+
+let users = [
+  { name: "John Doe", age: 34 },
+  { name: "Jane Doe", age: 33 },
+  { name: "Jack Smith", age: 54 }
+];
+
+app.get("/", (req, res) => {
+  res.render("index", user);
+});
+
+users = [];
+
+app.get('/users', (req,res) => {
+  res.render('users', {users:users});
 });
 
 app.listen(PORT, () => {
