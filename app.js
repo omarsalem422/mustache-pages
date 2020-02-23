9706bcdc538dfc8a345c621b60f5fcddd1d0d450
Omar <omar@omarsalempro.com>
2020-02-23 16:16:32 -0700

    Add code to implement session

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const path = require('path');
 const app = express();
 const mustacheExpress = require("mustache-express");
 const userRoutes = require("./routes/users");
+const session = require('express-session');
+
+app.use(session({
+  secret: 'Complex Formula',
+  resave: false,
+  saveUninitialized: true
+}));
 
 const VIEWS_PATH = path.join(__dirname, '/views');
 app.use(bodyParser.json() );
